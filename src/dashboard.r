10fc4a7ce09633b6597d29ef4c8817bec65cd496
Jitrayu Punrattanapongs <jitrayu.p@gmail.com>
2022-02-20 22:44:44 +0100

    feat: add theme and theme changer dropdown list to the sidebar

--- src/dashboard.r
+++ src/dashboard.r
@@ -1,18 +1,46 @@
 library(DT)
 library(shiny)
 library(shinydashboard)
+library(dashboardthemes)
+source("moduleChangeTheme.R")
 
 load("../output/fifa_22_tidydata_cleaned.Rdata")
 
-ui <- dashboardPage(           # FULL PAGE: don't touch!
-  dashboardHeader(),         # Header zone
-  dashboardSidebar(),        # Sidebar zone
-  dashboardBody()            # Body zone
+### UI
+ui <- dashboardPage(
+  
+  ### UI header
+  dashboardHeader(
+    
+    ### changing logo
+    title = shinyDashboardLogo(
+      theme = 'grey_light',
+      boldText = 'FIFA',
+      mainText = '22',
+      badgeText = 'Dashboard'
+    )
+  ),
+  
+  ### UI sidebar
+  dashboardSidebar(
+    
+    ### Theme dropddown
+    uiChangeThemeDropdown()
+  ),
+  
+  ### UI body
+  dashboardBody(
+    
+    ### changing theme
+    uiChangeThemeOutput()
+  )
 )  
 
-server <- function(input, output){  # Server: computations!
+server <- function(input, output){
   
+  ## Changing Theme
+  callModule(module = serverChangeTheme, id= 'moduleChangeTheme')
 }
 
 # Run the app ----
-shinyApp(ui = ui, server = server)  # Aggregates the app.
+shinyApp(ui = ui, server = server)
